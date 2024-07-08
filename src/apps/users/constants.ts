fb5e3bd1e5e57d65ac9268a276b55c467b49f13f
Karan Chettri <karan.c@cumulations.com>
2024-07-08 10:44:33 +0530

    Adding migrations

---
+++ src/apps/users/constants.ts
@@ -0,0 +1,2 @@
+export const user_roles = ["admin", "user"];
+export const user_types = ["management", "tenant"];
--- src/database/migrations/common/20240707180202_create_user_table.ts
+++ src/database/migrations/common/20240707180202_create_user_table.ts
@@ -1,15 +1,31 @@
-import { Knex } from 'knex';
-import { logger } from '../../../setup';
+import { Knex } from "knex";
+import { logger } from "../../../setup";
+import { user_roles, user_types } from "../../../apps/users/constants";
 
 const tenantSchema = process.env.TENANT_SCHEMA || "public";
-logger.debug("Running migration in schema:", tenantSchema)
+logger.debug("Running migration in schema:", tenantSchema);
 
 exports.up = async (knex: Knex) => {
-// TODO: write migration here
-return knex.schema.withSchema(tenantSchema)
+  // TODO: write migration here
+  await knex.raw(
+    "CREATE TYPE user_role AS ENUM ('admin', 'user', 'org_owner')"
+  );
+  return knex.schema.withSchema(tenantSchema).createTable("users", (table) => {
+    table.uuid("id").primary().defaultTo(knex.raw("uuid_generate_v4()"));
+    table.string("name", 100).notNullable();
+    table.string("email", 100).unique().notNullable();
+    table.string("password_hash", 128).notNullable();
+    table.string("display_picture", 255).nullable();
+    table.enum("user_role", user_roles).nullable();
+    table.enum("user_type", user_types).nullable();
+
+    table.timestamps(true, true);
+
+    table.index("email");
+  });
 };
 
 exports.down = async (knex: Knex) => {
-// TODO: write rollback here
-return knex.schema.withSchema(tenantSchema)
+  // TODO: write rollback here
+  return knex.schema.withSchema(tenantSchema).dropTableIfExists("users");
 };
